4f3c8e1b7d234b466deac7401e4eabcf49683327
Leonie Straßer <leonie.strasser@yahoo.de>
2022-05-22 21:43:16 +0200

    NPC Spawner

---
+++ Assets/Scripts/Gäste/NPC_Spawner.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Spawner : MonoBehaviour
+{
+    [Tooltip("Hier das Prefab reinziehen, das gespawnt werden soll!")]
+    public GameObject spawnPrefab;
+    [Tooltip("Die mindestzeit, die zwischen zwei Spawns vergehen soll.")]
+    public int minTime;
+    [Tooltip("Die maximalzeit, die zwischen zwei Spawns vergehen soll.")]
+    public int maxTime;
+
+    int timeToNextSPawn;
+
+    bool npcSpawn;
+
+    private void Start()
+    {
+        npcSpawn = false;
+        SetRandomSpawntime();
+    }
+
+    private void Update()
+    {
+        if(npcSpawn == false)
+        {
+            npcSpawn = true;
+            SetRandomSpawntime();
+            StartCoroutine(SpawnerTime());
+        }
+    }
+
+    IEnumerator SpawnerTime ()
+    {
+        yield return new WaitForSeconds(timeToNextSPawn);
+        Instantiate(spawnPrefab, this.transform.position, this.transform.rotation); // Spawne neuen NPC
+        npcSpawn = false;
+    }
+
+    void SetRandomSpawntime()
+    {
+        timeToNextSPawn = Random.Range(minTime, maxTime);
+    }
+}

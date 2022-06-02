18a43b9c0d8de3b3fdd38a808a31324a8f55a4b9
Y00Jeongyeon <105708710+Y00Jeongyeon@users.noreply.github.com>
2022-06-02 16:07:55 +0200

    SoundNpcHörbar

---
+++ Assets/Scripts/SoundManagerScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    public Sounds[] sounds;
+
+    void Awake ()
+    {
+        foreach (Sounds s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+
+        }
+
+    }
+
+    public void Play (string name)
+    {
+        Sounds s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
+
+
+//bei npc script wo npc spawnt FindObjectType<SoundManager>().Play("spawnNPC");

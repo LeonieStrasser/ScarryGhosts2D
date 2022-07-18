4eb3ca3385303584acc43f03413e24c176ec35a1
Leonie Straßer <leonie.strasser@yahoo.de>
2022-07-18 15:47:24 +0200

    vfx implementation

--- Assets/Scripts/Geister/Ghost.cs
+++ Assets/Scripts/Geister/Ghost.cs
@@ -25,13 +25,21 @@ public class Ghost : MonoBehaviour
     [SerializeField]
     string[] spawnSounds;
 
+    // VFX
+    [SerializeField]
+    GameObject dieVFX;
+    [SerializeField]
+    ParticleSystem dustVFX;
+    [SerializeField]
+    ParticleSystem localDustVFX;
+
     private void Awake()
     {
         audioManager = FindObjectOfType<AudioScript>();
     }
     void Start()
     {
-        
+
         audioManager.Initialize3dSound(this.gameObject, out mySounds);
 
         GoToRandomTarget();
@@ -39,10 +47,19 @@ public class Ghost : MonoBehaviour
         //Audio
         audioManager.Play3dSoundAtMySource("GhostMoving", mySounds);
 
-        if(breakeOut)
+        if (breakeOut)
+        {
             audioManager.PlayOneOfThese3DSounds(breakeOutSounds, mySounds);
-        else if(soulSpawn)
+            GameObject vfx = Instantiate(dieVFX, dieVFX.transform.position, Quaternion.identity);
+            vfx.SetActive(true);
+        }
+        else if (soulSpawn)
             audioManager.PlayOneOfThese3DSounds(spawnSounds, mySounds);
+        {
+            audioManager.PlayOneOfThese3DSounds(breakeOutSounds, mySounds);
+            GameObject vfx = Instantiate(dieVFX, dieVFX.transform.position, Quaternion.identity);
+            vfx.SetActive(true);
+        }
 
     }
 
@@ -55,7 +72,7 @@ public class Ghost : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Guest")
+        if (other.tag == "Guest")
         {
             anim.SetTrigger("shock");
             audioManager.Play3dSoundAtMySource("GhostScare", mySounds);
@@ -64,9 +81,21 @@ public class Ghost : MonoBehaviour
 
     public void DestroyMe()
     {
+        dieVFX.transform.SetParent(null);
+        dieVFX.SetActive(true);
+        Destroy(dieVFX, 5);
+
+        dustVFX.transform.SetParent(null);
+        dustVFX.Stop();
+        Destroy(dustVFX.gameObject, 10);
+
+        localDustVFX.transform.SetParent(null);
+        localDustVFX.Stop();
+        Destroy(localDustVFX.gameObject, 10);
+
         Destroy(gameObject);
     }
-    
+
     public void BreakeOut()
     {
         breakeOut = true;
